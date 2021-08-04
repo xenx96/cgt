772c2cef437c179262350f92938550244f822d0d
root <root@ip-172-26-7-177.ap-northeast-2.compute.internal>
2021-08-05 00:27:07 +0900

    a

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,21 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//JAVA Version = 11
+//Jar, Gradle : Not Maven
+//Spring Boot : 2.5.3
+//Main Application 
+//여기에 뭘 추가 해야 할까요?
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		//Test Hello World! 2개 출력해보기 : 작성자(YDS) 2021-08-05
 		System.out.println("Hello, World!");
+		System.out.println("Hello, World!2");
+		//성공
 		
 	}
 
