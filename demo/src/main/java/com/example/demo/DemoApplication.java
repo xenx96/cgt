540de5023774c31123a4fd091d9870367915050c
root <root@ip-172-26-7-177.ap-northeast-2.compute.internal>
2021-08-05 00:13:37 +0900

    a

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.test.demo;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,8 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Hellow world!");
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Hello, World!");
+		
 	}
 
 }
