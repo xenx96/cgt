cdc386ace63e2ea6e18aa2ee64e5c5c0ecf808e0
xenx96 <xenx96@naver.com>
2021-09-17 09:49:56 +0900

    Login 기본로직 완성. react

--- cgt_react/src/components/login.js
+++ cgt_react/src/components/login.js
@@ -1,18 +1,29 @@
 import axios from 'axios';
 import React from 'react';
 class Login extends React.Component{
-    state = {};
-    handlelogin(){
+    state = {warningMessage : ""};
+    handleloginsubmit = async(e) =>{
+        const {id, pw} = e.target ;
 
+        const form = {ID : id.value,
+                    PW : pw.value
+        };
+        let res = await axios.post("/api/login",form,
+          {headers: { "Content-Type": `application/json`}}
+        );
+        await res.data?alert("로그인 되었습니다."):this.setState({
+            warnigMessage : "ID 혹은 PW를 다시 확인해주세요."
+        })
+        res.data?window.location.href("./main"):false;
     };
 
     render(){
         return(
-            <form submit = {this.handlelogin}>
+            <form submit = {this.handleloginsubmit}>
                     <div>아이디
                         <input type ='text' name = "id"/>
                         비밀번호
-                        <input type ='password' name = 'pw'/>
+                        <input type ='password' name = 'pw'/>{this.state.warningMessage}
                     </div>
             </form>
         )
